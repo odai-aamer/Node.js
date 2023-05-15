1637cbc14cc9e92faf882311d40c902f8ef4ab93
Odai Amer <129774290+odai-aamer@users.noreply.github.com>
2023-05-15 16:27:55 +0400

    Update product.js

--- controllers/product.js
+++ controllers/product.js
@@ -16,6 +16,7 @@ exports.getCreativeProducts = (req, res, next) => {
   CreativeProducts.fetchAll(products => {
     console.log(products);
     res.status(200).json({
+      products,
       products
     });
 });
